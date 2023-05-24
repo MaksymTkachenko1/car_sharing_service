e7b589926dcfa8fc9070f8be57519b9ebd0c35e4
MaksymTkachenko1 <maksym.tkachenko36@gmail.com>
2023-05-24 16:11:38 +0300

    Import Figma UI Code

---
+++ UI/maksym_s_application/app/src/test/java/com/maksymsapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.maksymsapplication.app
+
+import com.maksymsapplication.app.appcomponents.base.BaseActivity
+import com.maksymsapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
